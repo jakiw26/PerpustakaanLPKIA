b2676338b04b3af60c11c835c05ab2e5a633adc0
Dzaki Ahmad Fauzan <dzakiahmadfa123@gmail.com>
2026-05-11 13:10:52 +0700

    membuat migration table

---
+++ database/migrations/2026_05_11_060457_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('category_id');
+            $table->integer('author_id');
+            $table->integer('publisher_id');
+            $table->string('title');
+            $table->string('isbn')->nullable();
+            $table->year('year');
+            $table->integer('stock')->default(0);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
