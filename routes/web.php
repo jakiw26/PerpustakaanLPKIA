b2003956b074f25daaf23b80f182445e90775b50
Raka099 <rakamulfaidzy5@gmail.com>
2026-05-14 18:09:33 +0700

    crud members

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,21 @@ Route::get('/User', [UserController::class, 'index']);
 Route::post('/User/store', [UserController::class, 'store']);
 Route::put('/User/update/{id}', [UserController::class, 'update']);
 Route::delete('/User/delete/{id}', [UserController::class, 'delete']);
+
+//categories
+Route::get('/admin/categories', [CategoriesController::class, 'index']);
+Route::post('/categories/store', [CategoriesController::class, 'store']);
+Route::put('/categories/update/{id}', [CategoriesController::class, 'update']);
+Route::delete('/categories/delete/{id}', [CategoriesController::class, 'destroy']);
+
+//authors
+Route::get('/admin/authors', [AuthorsController::class, 'index']);
+Route::post('/authors/store', [AuthorsController::class, 'store']);
+Route::put('/authors/update/{id}', [AuthorsController::class, 'update']);
+Route::delete('/authors/delete/{id}', [AuthorsController::class, 'destroy']);
+
+//publishers
+Route::get('/admin/publishers', [PublishersController::class, 'index']);
+Route::post('/publishers/store', [PublishersController::class, 'store']);
+Route::put('/publishers/update/{id}', [PublishersController::class, 'update']);
+Route::delete('/publishers/delete/{id}', [PublishersController::class, 'destroy']);
